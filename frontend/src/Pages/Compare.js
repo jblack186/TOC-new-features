f573e0aa12b4c323df3f5bfeec1c2a61e21f3a1a
Jamison T Blackwell <blackwellj1040@gmail.com>
2021-08-15 19:03:33 -0400

    modal is functional

--- frontend/src/Pages/Compare.js
+++ frontend/src/Pages/Compare.js
@@ -1,53 +1,72 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import "../css/Compare.scss";
-import axios from "axios";
 import BrandItem from "../components/BrandItem";
 import Modal from "../components/Modal";
-import mockData from '../brandJSON';
+import mockData from "../brandJSON";
+import Escape from '../img/close.svg';
+
 
 const Compare = () => {
-  const [brandChoice, setBrandChoice] = useState('');
+  const [brandChoice, setBrandChoice] = useState("");
   const [brandId, setBrandId] = useState();
 
   function babyBrand(e) {
+    e.preventDefault()
     setBrandChoice(e.target.value)
-    // axios.get(`https://api.spoonacular.com/food/products/search?apiKey=648c0fa98bff450295c089d1a791f846&query=${e.target.value}`)
-    .then(res => {
-      console.log('respose', res)
-    })
-    .catch(err => {
-      console.log('err',err)
-    })
-
-
+      // axios.get(`https://api.spoonacular.com/food/products/search?apiKey=648c0fa98bff450295c089d1a791f846&query=${e.target.value}`)
+      .then((res) => {
+        console.log("respose", res);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
   }
 
-  function getId(e) {
-
+   function resetBrand() {
+    setBrandId("");
+    console.log(brandId)
   }
 
-  console.log('id', brandId)
+
   return (
     <div className="compare">
       <section className="compare__text">
-      <h3>Tiny O products contain no added sugars or salt.</h3>
-      <p>Can your go-to brands say the same? Click one of these popular brands to check.</p>
+     {brandId && <img onClick={resetBrand} className="escape" src={Escape} alt="escape" />}
+        <h3>Tiny O products contain no added sugars or salt.</h3>
+        <p>
+          Can your go-to brands say the same? Click one of these popular brands
+          to check.
+        </p>
       </section>
       <section className="compare__buttons">
-      <button onClick={babyBrand} value='Gerber'>Gerber</button>
-      <button onClick={babyBrand} value='Beech Nut Baby Food'>Beech Nut</button>
-      <button onClick={babyBrand} value='Plum'>Plum</button>
-      <button onClick={babyBrand} value='Earths Best Baby Foods'>Earth's Best Baby Foods</button>
-
+        <button onClick={babyBrand} value="Gerber">
+          Gerber
+        </button>
+        <button onClick={babyBrand} value="Beech Nut Baby Food">
+          Beech Nut
+        </button>
+        <button onClick={babyBrand} value="Plum">
+          Plum
+        </button>
+        <button onClick={babyBrand} value="Earths Best Baby Foods">
+          Earth's Best Baby Foods
+        </button>
       </section>
       <section className="compare__brand-items">
-       { mockData.products.map(item => {
-          return <div onClick={() => {setBrandId(item.id)}} className="compare__brand-items__flex"><BrandItem title={item.title} image={item.image}/>   {brandId === item.id &&<Modal  getId={getId} id={brandId}/>}
-          </div>
-        })
-      }
-      
-     </section>
+        {mockData.products.map((item) => {
+          return (
+            <div
+              onClick={() => {
+                setBrandId(item.id);
+              }}
+              className="compare__brand-items__flex"
+            >
+              <BrandItem title={item.title} image={item.image} />
+              {brandId === item.id ? <Modal itemName={item.title} resetBrand={resetBrand} escape={escape} id={item.id} /> : null}
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 };
--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import '../css/Modal.scss';
+import axios from "axios";
+import mockData from "../brandJSON";
 
 function Modal(props) {
+  const [sodium, setSodium] = useState()
+  const [sugar, setSugar] = useState()
+  const [brandItem, setBrandItem] = useState()
+console.log(props)
+
+  useEffect(() => {
+    axios.get('')
+    let data = mockData.ingredients.find(item => {
+      return (item.id = props.getId)
+    })
+    setBrandItem(data);
+
+  }, [props])
 
 
   return (
